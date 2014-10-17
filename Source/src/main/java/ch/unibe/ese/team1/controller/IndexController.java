f01561319dc9eb4a6ca29f82161213c619a05351
Raphael Bucher <bucherraphael@gmx.ch>
2014-10-17 16:18:36 +0200

    nut much

--- Source/src/main/java/ch/unibe/ese/team1/controller/IndexController.java
+++ Source/src/main/java/ch/unibe/ese/team1/controller/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ch.unibe.ese.team1.controller.pojos.PlaceAdForm;
+
 @Controller
 public class IndexController {
 
@@ -29,7 +31,7 @@ public class IndexController {
 	public ModelAndView placeAd(){
 		
 		ModelAndView model = new ModelAndView("placeAd");
-		//model.addObject("signupForm", new SignupForm());    
+		model.addObject("placeAdForm", new PlaceAdForm());    
 		return model;
 	}
 }
