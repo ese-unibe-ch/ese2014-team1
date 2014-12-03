52b577925db128fd177d0cf905c5280eef60a932
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2014-12-03 18:17:01 +0100

    Fixed bookmark bug
    bookmarks are now deleted again

--- Source/src/main/java/ch/unibe/ese/team1/model/Ad.java
+++ Source/src/main/java/ch/unibe/ese/team1/model/Ad.java
@@ -343,4 +343,26 @@ public class Ad{
 		this.visits = visits;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ad other = (Ad) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 }
